c61ceac9034345d0b011dd5b5b9bc47b82e3fe82
Saeed Afzal <37543494+saeedafzal@users.noreply.github.com>
2025-05-08 17:41:31 +0100

    Indent JSON responses.

--- tui/resty/response_body.go
+++ tui/resty/response_body.go
@@ -1,6 +1,13 @@
 package resty
 
-import "github.com/saeedafzal/resty/model"
+import (
+	"encoding/json"
+	"strings"
+	"bytes"
+	"log/slog"
+
+	"github.com/saeedafzal/resty/model"
+)
 
 func (r Resty) initResponseBodyTextView() {
 	textview := r.responseBodyTextView.
@@ -14,5 +21,18 @@ func (r Resty) initResponseBodyTextView() {
 }
 
 func (r Resty) updateResponseBody(res *model.ResponseData) {
-	r.responseBodyTextView.SetText(res.Body)
+	displayData := res.Body
+	contentType := res.Headers.Get("Content-Type")
+
+	// If json, indent it properly for display.
+	if strings.Contains(contentType, "application/json") {
+		var dst bytes.Buffer
+		if err := json.Indent(&dst, []byte(displayData), "", "  "); err != nil {
+			slog.Warn("Failed to json indent response:", "content-type", contentType, "data", res.Body)
+		} else {
+			displayData = dst.String()
+		}
+	}
+
+	r.responseBodyTextView.SetText(displayData)
 }
