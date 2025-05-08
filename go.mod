4c738fdb25e285bd835be59ffd4f3f45c98ba2cd
Saeed Afzal <37543494+saeedafzal@users.noreply.github.com>
2025-05-08 21:23:29 +0100

    Add syntax highlighting to the response body.

--- go.mod
+++ go.mod
@@ -3,16 +3,18 @@ module github.com/saeedafzal/resty
 go 1.24
 
 require (
+	github.com/alecthomas/chroma/v2 v2.17.2
 	github.com/gdamore/tcell/v2 v2.8.1
 	github.com/saeedafzal/tview v1.0.0
 )
 
 require (
+	github.com/dlclark/regexp2 v1.11.5 // indirect
 	github.com/gdamore/encoding v1.0.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/sys v0.32.0 // indirect
-	golang.org/x/term v0.31.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 )
--- tui/resty/response_body.go
+++ tui/resty/response_body.go
@@ -5,8 +5,16 @@ import (
 	"strings"
 	"bytes"
 	"log/slog"
+	"io"
+	"errors"
 
 	"github.com/saeedafzal/resty/model"
+
+	"github.com/saeedafzal/tview"
+
+	"github.com/alecthomas/chroma/v2/lexers"
+	"github.com/alecthomas/chroma/v2/styles"
+	"github.com/alecthomas/chroma/v2/formatters"
 )
 
 func (r Resty) initResponseBodyTextView() {
@@ -24,8 +32,11 @@ func (r Resty) updateResponseBody(res *model.ResponseData) {
 	displayData := res.Body
 	contentType := res.Headers.Get("Content-Type")
 
+	lexer := lexers.Fallback
+
 	// If json, indent it properly for display.
 	if strings.Contains(contentType, "application/json") {
+		lexer = lexers.Get("json")
 		var dst bytes.Buffer
 		if err := json.Indent(&dst, []byte(displayData), "", "  "); err != nil {
 			slog.Warn("Failed to json indent response:", "content-type", contentType, "data", res.Body)
@@ -34,5 +45,23 @@ func (r Resty) updateResponseBody(res *model.ResponseData) {
 		}
 	}
 
-	r.responseBodyTextView.SetText(displayData)
+	// Syntax highlighting on response body text.
+	style := styles.Get("monokai")
+	if style == nil {
+		style = styles.Fallback
+	}
+
+	formatter := formatters.Get("terminal16m")
+	if formatter == nil {
+		formatter = formatters.Fallback
+	}
+
+	content, err1 := io.ReadAll(strings.NewReader(displayData))
+	iterator, err2 := lexer.Tokenise(nil, string(content))
+
+	r.responseBodyTextView.Clear()
+	w := tview.ANSIWriter(r.responseBodyTextView)
+	if err3 := formatter.Format(w, style, iterator); errors.Join(err1, err2, err3) != nil {
+		r.responseBodyTextView.SetText(displayData)
+	}
 }
